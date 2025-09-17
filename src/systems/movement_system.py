0947af078e1ff0604ad9cceb870961383871a9c4
joshuaj21 <joshuajung360@gmail.com>
2025-09-17 20:58:12 +0900

    10

--- src/systems/movement_system.py
+++ src/systems/movement_system.py
@@ -5,6 +5,7 @@ from typing import TYPE_CHECKING
 from core.system import ISystem
 from components.position_component import PositionComponent
 from components.velocity_component import VelocityComponent
+from components.player_component import PlayerComponent
 
 if TYPE_CHECKING:
     from core.entity_manager import EntityManager
@@ -18,9 +19,29 @@ class MovementSystem(ISystem):
         """
         Updates the position of all entities with a velocity.
         """
+        # Handle player debuffs first
+        for player_entity in entity_manager.get_entities_with_components(PlayerComponent, VelocityComponent):
+            player_comp = entity_manager.get_component(player_entity.id, PlayerComponent)
+            if player_comp.slow_debuff_stacks > 0:
+                player_comp.slow_debuff_timer -= delta_time
+                if player_comp.slow_debuff_timer <= 0:
+                    player_comp.slow_debuff_stacks = 0
+                    player_comp.slow_debuff_timer = 0
+
+        # Generic movement
         for entity in entity_manager.get_entities_with_components(PositionComponent, VelocityComponent):
             pos = entity_manager.get_component(entity.id, PositionComponent)
             vel = entity_manager.get_component(entity.id, VelocityComponent)
+            
+            final_dx = vel.dx
+            final_dy = vel.dy
 
-            pos.x += vel.dx * delta_time
-            pos.y += vel.dy * delta_time
+            if entity_manager.has_component(entity.id, PlayerComponent):
+                player_comp = entity_manager.get_component(entity.id, PlayerComponent)
+                if player_comp.slow_debuff_stacks > 0:
+                    reduction = 0.2 * player_comp.slow_debuff_stacks
+                    final_dx *= (1.0 - reduction)
+                    final_dy *= (1.0 - reduction)
+
+            pos.x += final_dx * delta_time
+            pos.y += final_dy * delta_time
